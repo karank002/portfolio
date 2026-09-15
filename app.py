fc02dde59f7e34a536a9b5c400c30c9854ee5b9e
Karan Singh <karankanwal835@gmail.com>
2026-09-15 19:47:55 +0530

    initial portfolio setup

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.set_page_config(
+    page_title = "Karan Singh | Data Science",
+    page_icon="📊",
+    layout="wide",
+)
+st.title("Karan Singh")
+st.subheader("Data Science student")
+
+st.write(
+    "Building projects in Data Science, Machine Learning, "
+    "Big Data and Backend Systems."
+)
